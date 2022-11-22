f59a26f931750d5e1fa7901f17c34d1924a0e2fa
Aliza O'Connor <aliza.oconnor@jbhunt.com>
2022-11-22 16:05:34 -0600

    created 2 components with input for them to communicate

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { RoomList, Rooms } from './rooms';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent implements OnInit {
+
+  hotelName= 'Hilton Hotel';
+  numberOfRooms=10;
+  hideRooms=false;
+
+  rooms: Rooms={
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+  roomList:RoomList[]=[];
+  
+   constructor() { }
+
+  ngOnInit(): void {
+    this.roomList =[
+      {roomNumber: 1,
+       roomType: 'Deluxe Room',
+       amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+       price: 500,
+       photos: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=',
+       checkinTime: new Date('11-Nov-2021'),
+       checkoutTime: new Date('12-Nov-2021'),
+       rating:5,
+     },
+     {roomNumber: 2,
+       roomType: 'Deluxe Room',
+       amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+       price: 1000,
+       photos: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=',
+       checkinTime: new Date('11-Nov-2021'),
+       checkoutTime: new Date('12-Nov-2021'),
+       rating: 3.4,
+     },
+     { roomNumber: 3,
+       roomType: 'Private Suite',
+     amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+     price: 15000,
+     photos: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=',
+     checkinTime: new Date('11-Nov-2021'),
+     checkoutTime: new Date('12-Nov-2021'),
+     rating: 2.6,
+   },
+     ]
+   
+  }
+
+  toggle(){
+    this.hideRooms= !this.hideRooms;
+
+  }
+
+}
