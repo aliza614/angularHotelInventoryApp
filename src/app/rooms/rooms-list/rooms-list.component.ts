9b827395fae0d28364112df96faa4f024043cfb1
Aliza O'Connor <aliza.oconnor@jbhunt.com>
2022-11-23 09:30:48 -0600

    adding change detection back in

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit, Output } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input, OnInit, Output } from '@angular/core'
 import { EventEmitter } from '@angular/core';
 import { RoomList } from '../rooms'
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RoomsListComponent implements OnInit {
 
